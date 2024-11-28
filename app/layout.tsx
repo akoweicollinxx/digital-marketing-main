72fc0df5be876181becec6a8709f4c2b69c2c421
akoweicollinxx <110723714+akoweicollinxx@users.noreply.github.com>
2024-11-28 11:12:12 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Analytics } from "@vercel/analytics/react";
 const font = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bird | Design & Development Studio",
+  title: "New Wrld | Design & Development Studio",
   description: "Grow your business with Bird, a design & development studio.",
   openGraph: {
     images: [
